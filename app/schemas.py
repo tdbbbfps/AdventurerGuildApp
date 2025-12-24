9a837664da098c9dcc388fc7168d908da82ed211
tdbbb <tdbbb0427@gmail.com>
2025-12-24 16:48:53 +0800

    refactor: refactor class to job.

--- app/schemas.py
+++ app/schemas.py
@@ -2,28 +2,28 @@ from pydantic import BaseModel, Field
 
 class AdventurerBase(BaseModel):
     name : str = Field(..., min_length=3, max_length=50, description="adventurer's name.")
-    class_id : int = Field(..., description="adventurer's class.")
+    job_id : int = Field(..., description="adventurer's job.")
     level : int = Field(..., description="adventurer's level.", ge=1)
     is_active : bool = Field(description="adventurer's status.", default=True)
     gold : int = Field(ge=0, description="adventurer's gold.", default=0)
 
 class AdventurerCreate(AdventurerBase):
     pass
+        
+class JobBase(BaseModel):
+    name : str = Field(..., min_length=3, max_length=50, description="Job's name.")
 
-class Adventurer(AdventurerBase):
+class JobCreate(JobBase):
+    pass
+
+class Job(JobBase):
     id : int
 
     class Config:
         from_attributes = True
-        
-class ClassBase(BaseModel):
-    name : str = Field(..., min_length=3, max_length=50, description="class's name.")
 
-class ClassCreate(ClassBase):
-    pass
-
-class Class(ClassBase):
+class Adventurer(AdventurerBase):
     id : int
-
+    job : Job
     class Config:
         from_attributes = True
