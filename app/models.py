b23b6debb91c45c2dfb856e9442c941d4aa6f4ba
tdbbb <tdbbb0427@gmail.com>
2025-12-24 17:11:38 +0800

    docs: add annotations.

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class Adventurer(Base):
     level = Column(Integer, default=1)
     is_active = Column(Boolean, default=True)
     gold = Column(Integer, default=0)
-    
+    # Add relationship
     job = relationship("Job", back_populates="adventurers")
     
 class Job(Base):
@@ -19,5 +19,5 @@ class Job(Base):
     
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False, index=True, unique=True)
-    
-    adventurers = relationship("Adventurer", back_populates="jobs")
+    # Add relationship
+    adventurers = relationship("Adventurer", back_populates="job")
