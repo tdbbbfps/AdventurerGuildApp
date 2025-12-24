5da7aaa7155ecb7badd9250ddff12f387eada892
tdbbb <tdbbb0427@gmail.com>
2025-12-24 17:11:08 +0800

    refactor: move function get_database to database.py.

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 import models, schemas, database
-from jobs_router import jobs_router
+from jobs_router import router as jobs_router
 import uvicorn
 
 models.Base.metadata.create_all(bind=database.engine)
@@ -12,21 +12,13 @@ app = FastAPI(
     version="1.0.0",
 )
 app.include_router(jobs_router)
-
-# Dependency: Get database connection.
-def get_database():
-    db = database.session()
-    try:
-        yield db
-    finally:
-        db.close()
         
 @app.get("/")
 async def root():
     return {"message": "這裡是冒險者公會後端API的根目錄。"}
 
 @app.post("/adventurers/create", response_model=schemas.Adventurer)
-async def create_adventurer(adventurer : schemas.AdventurerCreate, db : Session = Depends(get_database)):
+async def create_adventurer(adventurer : schemas.AdventurerCreate, db : Session = Depends(database.get_database)):
     db_adventurer = models.Adventurer(
         name = adventurer.name,
         job_id = adventurer.job_id,
@@ -34,23 +26,34 @@ async def create_adventurer(adventurer : schemas.AdventurerCreate, db : Session 
         is_active = adventurer.is_active,
         gold = adventurer.gold
     )
-    database.add(db_adventurer)
-    database.commit()
-    database.refresh(db_adventurer)
+    db.add(db_adventurer)
+    db.commit()
+    db.refresh(db_adventurer)
     return db_adventurer
 
+@app.put("/adventurers/update", response_model=schemas.Adventurer)
+async update_adventurer(adventurer : AdventurerUpdate, db : Session = Depends(database.get_database)):
+    db.execute(text("UPDATE adventurers set name = :name, job_id = :job_id, level = :level, is_active = :is_active where id = :id"), (
+        adventurer.id,
+        adventurer.name,
+        adventurer.job_id,
+        adventurer.level,
+        adventurer.is_active
+    ,))
+    
+    return
 @app.get("/adventurers/read", response_model=list[schemas.Adventurer])
-def read_adventurers(skip : int = 0, limit : int = 100, db : Session = Depends(get_database)):
+def read_adventurers(skip : int = 0, limit : int = 100, db : Session = Depends(database.get_database)):
     adventurers = db.query(models.Adventurer).offset(skip).limit(limit).all()
     return adventurers
 
 @app.get('/adventurers/read_level', response_model=list[schemas.Adventurer])
-async def read_adventurers_level_ge(ge : int = 0, db : Session = Depends(get_database)):
+async def read_adventurers_level_ge(ge : int = 0, db : Session = Depends(database.get_database)):
     adventurers = db.execute(text("""SELECT * FROM adventurers where level >= :ge"""), {"ge" : ge}).fetchall()
     return adventurers
 
 @app.get('/adventurers/average_level')
-async def get_adventurers_average_level(db : Session = Depends(get_database)):
+async def get_adventurers_average_level(db : Session = Depends(database.get_database)):
     sql = text("""
                 SELECT 
                     ROUND(AVG(a.level), 0) as Average Level
