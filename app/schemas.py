afad40864718128c27b4113678716cd101be6a21
tdbbb <tdbbb0427@gmail.com>
2025-12-24 17:11:53 +0800

    feat: add a model for updating adventurer.

--- app/schemas.py
+++ app/schemas.py
@@ -5,11 +5,19 @@ class AdventurerBase(BaseModel):
     job_id : int = Field(..., description="adventurer's job.")
     level : int = Field(..., description="adventurer's level.", ge=1)
     is_active : bool = Field(description="adventurer's status.", default=True)
-    gold : int = Field(ge=0, description="adventurer's gold.", default=0)
+    gold : int = Field(description="adventurer's gold.", ge=0, default=0)
 
 class AdventurerCreate(AdventurerBase):
     pass
-        
+
+class AdventurerUpdate(BaseModel):
+    id : int = Field(..., description="Adventurer's ID.")
+    name : str = Field(description="Adventurer's new name", min_length=3, max_length=50, nullable=True)
+    job_id : int = Field(description="Adventurer's new job.", nullable=True)
+    level : int = Field(description="Adventurer's new level.", ge=1, nullable=True)
+    is_active : bool = Field(description="Adventurer's new status.", nullable=True)
+    gold : int = Field(description="Adventurer's new gold.", ge=0, nullable=True)
+
 class JobBase(BaseModel):
     name : str = Field(..., min_length=3, max_length=50, description="Job's name.")
 
